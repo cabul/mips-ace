d417a4abaeef0a0c63d78b9b67597aae745d08e2
ediaz <quique.lpgc@gmail.com>
2015-10-12 18:24:48 +0200

    ALU is working. Testbench created.

--- alu.v
+++ alu.v
@@ -1,13 +1,17 @@
 `ifndef _alu
-`define _datapath
+`define _alu
 
 /*
- * This unit is asinchronous
+ * ALU info & pinout
+ * -----------------
  *
+ * This unit is asinchronous
+ * 
  * alu_op: alu operation
  * in_s: input s
  * in_t: input t
  * shamt: shift amount (ignored unless shift op)
+ * zero: if division by 0 (TODO check this and any other output needed)
  * out: alu ouput
  */
 
@@ -15,24 +19,29 @@ module alu(
 	input wire [5:0] alu_op,
 	input wire [N-1:0] in_s,
 	input wire [N-1:0] in_t,
-    input wire [N-1:0] shamt,
+	input wire [N-1:0] shamt,
+	output reg zero = 1'd0,
 	output reg [N-1:0] out = {N{1'b0}});
 
 parameter N = 32;
 
 always @* begin
-    // TODO check for overflow, div by 0 and unsigned operations
-    // TODO multiplications requiere two output channels (or a N*2 bus)
+	// TODO check for overflow, div by 0
+	// TODO implement unsigned operations
+	// TODO multiplications write to a special register LO and HI
+	// TODO multiplications and data hazards?
 	case (alu_op)
-	    6'b000000: out <= in_t << shamt;
-		6'b000011: out <= in_t >> shamt;
+		6'b000000: out <= in_t << shamt;
+		6'b000010: out <= in_t >> shamt;
+		6'b000011: out <= in_t >>> shamt; // TODO check operator
 		6'b100000: out <= in_s + in_t;
 		6'b100010: out <= in_s - in_t;
 		6'b100100: out <= in_s & in_t;
 		6'b100101: out <= in_s | in_t;
+		6'b100110: out <= in_s ^ in_t;
 		default: begin
-		    $display("Warning: ALU received unknown alu_op signal.");
-		    out <= N{1'b0};
+			$display("Warning: ALU received unknown alu_op signal.");
+			out <= {N{1'b0}};
 		end
 	endcase
 end
--- test/alu_tb.v
+++ test/alu_tb.v
@@ -2,8 +2,19 @@
 
 module alu_tb;
 
+wire zero;
+wire [31:0] out;
+
+alu alu(
+	.alu_op(6'b100000),
+	.in_s(32'd100),
+	.in_t(32'd130),
+	.shamt(32'd0),
+	.zero(zero),
+	.out(out));
+
 initial begin
-	$monitor("Success.");
+	$monitor("%d + %d = %d", alu.in_s, alu.in_t, out);
 end
 
-endmodule;
+endmodule
