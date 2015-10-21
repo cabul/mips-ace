cecbb9d35ad9b7cd7bd74e2858a4ad1b5d8bd479
Calvin <calvin.bulla@gmail.com>
2015-10-21 13:11:42 +0200

    Cleanup & Docs

--- src/multiplexer.v
+++ src/multiplexer.v
@@ -8,14 +8,14 @@
 ///
 module multiplexer(
 	input wire [$clog2(X)-1:0] select,
-	input wire [N*X-1:0] input_data,
-	output reg [N-1:0] output_data);
+	input wire [N*X-1:0] in_data,
+	output reg [N-1:0] out_data);
 
 parameter N = 32;
 parameter X = 2;
 
 always @* begin
-	output_data <= input_data[(select+1)*N-1-:N];
+	out_data <= in_data[(select+1)*N-1-:N];
 end
 
 endmodule
--- src/signextender.v
+++ src/signextender.v
@@ -11,11 +11,11 @@
 /// extended - output sign-extended register
 /// 
 module signextender(
-		input wire [15:0] extend,
-		output reg [31:0] extended);
+	input wire [15:0] extend,
+	output reg [31:0] extended);
 
 always @* begin
- 		extended[31:0] <= { {16{extend[15]}}, extend[15:0] };
+	extended[31:0] <= { {16{extend[15]}}, extend[15:0] };
 end
 endmodule
 `endif
