60ccf0e061b92463152a33a6e17ef41f100cfcbd
edroque93 <quique.lpgc@gmail.com>
2015-10-29 12:26:36 +0100

    Fixed ALU to follow the new design

--- src/alu.v
+++ src/alu.v
@@ -1,6 +1,8 @@
 `ifndef _alu
 `define _alu
 
+`include aludefines.v
+
 ///
 /// Algorithmetic Logic Unit
 /// 
@@ -9,41 +11,42 @@
 /// Ports:
 ///
 /// alu_op - alu operation
-/// in_s - input s
-/// in_t - input t
-/// shamt - shift amount (ignored unless shift op)
-/// zero - if division by 0 (TODO check this and any other output needed)
+/// s - input s
+/// t - input t
+/// shamt - shift amount, ignored unless shift operation
+/// zero - if division by 0
+/// overflow - if overflow
 /// out - alu ouput
 /// 
 module alu(
-	input wire [5:0] alu_op,
-	input wire [N-1:0] in_s,
-	input wire [N-1:0] in_t,
+	input wire [3:0] alu_op,
+	input wire [N-1:0] s,
+	input wire [N-1:0] t,
 	input wire [4:0] shamt,
 	output reg zero = 1'd0,
+    output reg overflow = 1'd0,
 	output reg [N-1:0] out = {N{1'b0}});
 
 parameter N = 32;
 
 always @* begin
-	// TODO check for overflow, div by 0
-	// TODO implement unsigned operations
-	// TODO multiplications write to a special register LO and HI
-	// TODO multiplications and data hazards?
 	case (alu_op)
-		6'b000000: out <= in_t << shamt;
-		6'b000010: out <= in_t >> shamt;
-		6'b000011: out <= in_t >>> shamt; // TODO check operator
-		6'b100000: out <= in_s + in_t;
-		6'b100010: out <= in_s - in_t;
-		6'b100100: out <= in_s & in_t;
-		6'b100101: out <= in_s | in_t;
-		6'b100110: out <= in_s ^ in_t;
-		default: begin
+        OP_SLL: out <= t << shamt;
+        OP_SRL: out <= t >> shamt;
+        OP_SRA: out <= t >>> shamt;
+        OP_ADD: out <= s + t;
+        OP_SUB: out <= s - t;
+        OP_AND: out <= s & t;
+        OP_OR:  out <= s | t;
+        OP_XOR: out <= s ^ t;
+        OP_NOR: out <= ~(s | t);
+        OP_SLT: out <= (s < t)? 32'd1 : 32'd0;
+		default:
 			$display("Warning: ALU received unknown alu_op signal.");
-			out <= {N{1'b0}};
-		end
 	endcase
+    
+    zero     <= (out == 32'd0) 1'b1 : 1'b0;
+    overflow <= 1'b0; // TODO fix overflow formula
 end
 
 endmodule
