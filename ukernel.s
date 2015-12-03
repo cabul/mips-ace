6af5d945aed62b6ba0cdd1f14c06a37b1a9f4602
edroque93 <quique.lpgc@gmail.com>
2015-12-03 16:50:23 +0100

    Update of ukernel and asm .space directive (only decimal+)

--- ukernel.s
+++ ukernel.s
@@ -10,27 +10,89 @@
 
 #.kdata
 .data
-str_exception:  .asciiz "[Exception] "
-save_reg:       .word 0, 0 # .space 8 not working, right? imsad.png
+str_exception:          .asciiz "Exception: "
+str_unimplemented:      .asciiz "exception not implemented raised.\n"
+save_reg:               .space 8
+exception_jumptable:	.word int, unimpl1, unimpl2, unimpl3, AdEL, AdES, IBE, DBE, Sys, Bp, RI, CpU, Ov, Tr, unimpl4, FPE
 
 #.ktext
 .text
-    li $k0, 13
-    li $k0, 0 # mfc0 $k0, $k0 # what the hell is going on dude
+    li $s0, 9
+    mtc0 $s0, $cause
+    mfc0 $s1, $cause
+    sw $0, %IO_EXIT($0)
+    
+    mfc0 $k0, $cause
     bne $k0, $0, exception_handler
     
 entry_point:
-    li $sp, 0x300
+    li $sp, 0x300               # Set stack-pointer
+    li $k0, 12
+    mtc0 $status, $0            # Set machine to user-mode
     jal main
     sw $0, %IO_EXIT($0)
 
 exception_handler:
 	la $k0, save_reg
-	sw $v0, 0($k0)          # Not re-entrant and we can't trust $sp
-	sw $a0, 4($k0)          # But we need to use these registers
+	sw $v0, 0($k0)              # Not re-entrant and we can't trust $sp
+	sw $a0, 4($k0)              # But we need to use these registers
     la $a0, str_exception
     jal kernel_strprint
-    sw $0, %IO_EXIT($0)     # I give up
+    mfc0 $k0, $cause            # Cause register
+    srl $a0, $k0, 2             # Extract ExcCode Field
+    andi $a0, $a0, 0x1F
+    sll $a0, $a0, 2
+    la $v0, exception_jumptable
+    add $v0, $v0, $a0
+    lw $v0, 0($v0)
+    jr $v0                      # Switch exception table (see table below)
+
+	# 0  - Int Interrupt (hardware)
+	# 4  - AdEL Address Error Exception (load or instruction fetch)
+	# 5  - AdES Address Error Exception (store)
+	# 6  - IBE Bus Error on Instruction Fetch
+	# 7  - DBE Bus Error on Load or Store
+	# 8  - Sys Syscall Exception
+	# 9  - Bp Breakpoint Exception
+	# 10 - RI Reserved Instruction Exception
+	# 11 - CpU Coprocessor Unimplemented
+	# 12 - Ov Arithmetic Overflow Exception
+	# 13 - Tr Trap
+	# 15 - FPE Floating Point Exception
+
+CpU:
+Bp:
+FPE:
+int:
+Tr:
+unimpl1:
+unimpl2:
+unimpl3:
+unimpl4:
+#
+AdEL:
+AdES:
+IBE:
+DBE:
+Sys:
+RI:
+Ov:
+#
+    la $a0, str_unimplemented
+    jal kernel_strprint
+    j epc
+
+epc:
+    mfc0 $a0, $epc
+    andi $a0, $a0, 0x3          # Is EPC word-aligned?
+    beq $a0, $0, ret_exception
+    sw $0, %IO_EXIT($0)         # Exit
+ret_exception:
+	la $k0, save_reg
+	lw $v0, 0($k0)		        # Restore other registers
+	lw $a0, 4($k0)
+	mtc0 $0, $cause		        # Clear Cause register
+	eret                        # Return
 
 # Note that kernel functions do not save registers
 
