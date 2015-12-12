7d189a81c689520e5774e721d4510d92e2dfaad2
barredator <adrian.barredof@gmail.com>
2015-12-12 19:51:58 +0100

    Store buffer modified to support external loads

--- src/stbuffer.v
+++ src/stbuffer.v
@@ -4,11 +4,12 @@
 `include "defines.v"
 
 module stbuffer(
-                input clk,
+                input             clk,
                 input wire        reset,
                 input wire [31:0] in_addr,
                 input wire [31:0] in_wdata,
                 input wire        store,
+                input wire        load,
                 input wire        isALUOp,
                 output reg [31:0] out_addr = 0,
                 output reg [31:0] out_wdata = 0,
@@ -23,22 +24,33 @@ integer i= 0;
 
 reg [WIDTH-1:0] stbuff [DEPTH-1:0];
 reg [2:0] counter = 0;
+reg [31:0] temp = 0;
+
 
 always @(posedge clk) begin
    if (reset) begin
      for (i = 0; i < DEPTH; i = i+1) begin
         stbuff[i] <= {WIDTH{1'b0}};
      end
+   end else if(load == 1) begin
+    for (i = 0; i < ELEMENTS; i = i+1) begin
+      if (stbuff[2*i] == in_addr)
+          temp <= stbuff[2*i+1];
+    end
+     out_wdata <= temp;
+     memwrite <= 0;
   end else if (store == 1) begin
       if (counter < ELEMENTS) begin
          stbuff[2*counter] <= in_addr;
          stbuff[2*counter+1] <= in_wdata;
          counter <= counter +1;
          stall <= 0;
+         memwrite <= 0;
       end else begin
          stall <= 1;
          out_addr <= stbuff[0];
          out_wdata <= stbuff[1];
+         memwrite <= 1;
          for (i = 0; i < 2*(ELEMENTS-1); i = i+1) begin
             stbuff[i] <= stbuff[i+2];
          end
@@ -48,6 +60,7 @@ always @(posedge clk) begin
   end else if (isALUOp == 1) begin
       out_addr <= stbuff[0];
       out_wdata <= stbuff[1];
+      memwrite <= 1;
       for (i = 0; i < 2*(ELEMENTS-1); i = i+1) begin
             stbuff[i] <= stbuff[i+2];
          end
@@ -56,7 +69,6 @@ always @(posedge clk) begin
       counter <= counter -1;
       stall <= 0;
   end
-   else stall <=0;
 end
 
 endmodule
--- test/stbuffer_tb.v
+++ test/stbuffer_tb.v
@@ -9,13 +9,13 @@ module stbuffer_tb;
    reg [31:0] tb_in_wdata = 0;
    reg        tb_store = 0;
    reg        tb_isALUOp = 0;
-
+   reg        tb_load = 0;
    wire [31:0] tb_out_addr;
    wire [31:0] tb_out_wdata;
    wire        tb_stall;
    wire        tb_memwrite;
 
-   stbuffer stbuffer(.clk(tb_clk), .reset(tb_reset), .in_addr(tb_in_addr), .in_wdata(tb_in_wdata), .store(tb_store), .isALUOp(tb_isALUOp), .out_addr(tb_out_addr), .out_wdata(tb_out_wdata), .stall(tb_stall), .memwrite(tb_memwrite));
+   stbuffer stbuffer(.clk(tb_clk), .reset(tb_reset), .in_addr(tb_in_addr), .in_wdata(tb_in_wdata), .store(tb_store), .isALUOp(tb_isALUOp), .out_addr(tb_out_addr), .out_wdata(tb_out_wdata), .stall(tb_stall), .memwrite(tb_memwrite), .load(tb_load));
 
 initial begin
 	`ifdef TRACEFILE
@@ -31,6 +31,7 @@ initial begin
    tb_in_wdata = 32'd1;
    tb_store = 1;
    tb_isALUOp = 0;
+                     tb_load = 0;
    end
 
    #5
@@ -39,14 +40,15 @@ initial begin
       #5 begin
          tb_clk =1;
 
- tb_in_addr = 32'd9;
-   tb_in_wdata = 32'd2;
-   tb_store = 1;
+ tb_in_addr = 32'd5;
+ //  tb_in_wdata = 32'd2;
+   tb_store = 0;
    tb_isALUOp = 0;
-
+ tb_load = 1;
       end
 
    #5 tb_clk = 0;
+
 
    #5 begin
       tb_clk=1;
@@ -63,17 +65,26 @@ initial begin
 
    #5
       tb_clk = 0;
-
+/*
    #5 begin
       tb_clk = 1;
       tb_isALUOp = 1;
       tb_store = 0;
+                     tb_load = 1;
+                     tb_in_addr = 32'd3;
    end
 
    #5 tb_clk = 0;
 
    #5 tb_clk = 1;
 
+   #5  tb_clk = 0;
+
+   #5 tb_clk = 1;
+
+   #5 tb_clk = 0;
+   #5 tb_clk = 1;
+*/
    #5 begin
       tb_clk = 0;
       $finish;
