775b155cac1845483869ec22236917fb7e129f70
cabul <calvin.bulla@gmail.com>
2015-12-28 13:21:59 +0100

    Modified TLB, added MIPS v3

--- src/tlb.v
+++ src/tlb.v
@@ -6,54 +6,65 @@
 module tlb (
 	input wire clk,
 	input wire reset,
+
 	input wire read_enable,
 	input wire write_enable,
+
+	input wire [31:0] index_in,
+	output reg [31:0] index_out,
+
 	input wire [31:0] vaddr,
 	output reg [31:0] paddr = 0,
 	output reg hit = 0,
+	output reg valid = 0,
+
 	input wire [31:0] wr_vaddr,
 	input wire [31:0] wr_paddr
 );
 
 parameter DEPTH = `TLB_DEPTH;
 parameter PAGE_SIZE = `PAGE_SIZE;
-parameter ALIAS = "TLB";
+parameter ALIAS = "tlb";
 
 localparam DB = $clog2(DEPTH);     // Depth bits
 localparam PB = $clog2(PAGE_SIZE); // Page bits
-localparam VB = 32 - DB - PB;      // Virtual tag bits
-localparam TB = 32 - PB;           // Physical tag bits
+localparam TB = 32 - PB;           // Tag bits
 
-wire [PB-1:0] offset = vaddr[PB-1:0];
-wire [DB-1:0] index  = vaddr[PB+DB-1:PB];
-wire [VB-1:0] vtag   = vaddr[31:PB+DB];
+integer i;
 
-wire [DB-1:0] wr_index = wr_vaddr[PB+DB-1:PB];
-wire [VB-1:0] wr_vtag  = wr_vaddr[31:PB+DB];
-wire [TB-1:0] wr_ptag  = wr_paddr[31-:TB];
+wire [TB-1:0] wr_ptag = wr_paddr[31-:TB];
+wire [TB-1:0] wr_vtag = wr_vaddr[31-:TB];
 
-reg [VB-1:0]    vtags [0:DEPTH-1];
-reg [TB-1:0]    ptags [0:DEPTH-1];
+wire [TB-1:0] vtag = vaddr[31-:TB];
+wire [PB-1:0] voff = vaddr[31-TB:0];
+
+reg [TB-1:0] vtags [0:DEPTH-1];
+reg [TB-1:0] ptags [0:DEPTH-1];
 reg [DEPTH-1:0] validbits = 0;
 
-wire [TB-1:0] ptag = ptags[index];
-wire hit_int = (vtags[index] == vtag) & validbits[index];
-
-always @* if (~read_enable | reset)
-	hit <= 0;
-else hit <= hit_int;
-
-always @* if (hit)
-	paddr <= { ptag, offset };
-else paddr <= 0;
+always @* if (read_enable & ~reset & ~hit) begin
+	for (i = 0; i < DEPTH & ~hit; i = i+1) begin
+		if (vtags[i] == vtag) begin
+			paddr     <= {ptags[i],voff};
+			index_out <= i;
+			hit       <= 1;
+			valid     <= validbits[i];
+		end
+	end
+end else begin
+	paddr     <= 0;
+	index_out <= 0;
+	hit       <= 0;
+	valid     <= 0;
+end
 
 always @(posedge clk) begin
 	if (reset) validbits = 0;
 	else if (write_enable) begin
-		vtags[wr_index] = wr_vtag;
-		ptags[wr_index] = wr_ptag;
-		validbits[wr_index] = 1;
-		`INFO(("[%s] Insert %x = %x", ALIAS, wr_vaddr, wr_paddr))
+		vtags[index_in] <= wr_vtag;
+		ptags[index_in] <= wr_ptag;
+		validbits[index_in] <= 1;
+		`INFO(("[%s] Fill %x <=> %x", ALIAS, wr_vtag, wr_ptag))
 	end
 end
 
