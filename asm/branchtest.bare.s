41b917bcc95d1eab948706e8ed3e16451ad3ed7a
edroque93 <quique.lpgc@gmail.com>
2016-01-21 02:48:44 +0100

    Upgraded bp test

--- asm/branchtest.bare.s
+++ asm/branchtest.bare.s
@@ -3,23 +3,23 @@
     
     .text
 main:
-	la $s0, a
+    la $s0, a
     la $s1, b
-	la $s2, c
+    la $s2, c
     li $s3, 100
     li $s4, 1
 loop:
     lw $t0, 0($s0) 
     lw $t1, 0($s1)
-    add $t0, $t1, $0
-    sw $t0, 0($s2)
     addi $s0, $s0, 4
     addi $s1, $s1, 4
-    addi $s2, $s2, 4
+    add $t0, $t1, $0
     addi $s4, $s4, 1
-	bne $s4, $s3, loop
+    sw $t0, 0($s2)
+    addi $s2, $s2, 4
+    bne $s4, $s3, loop
 
-	sw $0, %IO_EXIT($0)
+    sw $0, %IO_EXIT($0)
 
 	.data
 a: .space 400 
